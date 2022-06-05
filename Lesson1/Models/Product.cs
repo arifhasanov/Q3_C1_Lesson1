2f962f39a0ef78c5da860ea8f324177581667bef
arifhasanov@yahoo.com <37578820+arifhasanov@users.noreply.github.com>
2022-06-05 17:17:44 +0200

    Done

---
+++ Lesson1/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Lesson1.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string? Name { get; set; } = null;
+    public string? Image { get; set; } = null;
+}
